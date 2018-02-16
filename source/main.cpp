ce1d9ce202ee6774894833910ca166f296ce2c5a
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-16 07:15:53 +0100

    Start writing GPIO registers in earnest

--- source/main.cpp
+++ source/main.cpp
@@ -12,11 +12,21 @@
 MicroBit uBit;
 
 int main(void) {
-    volatile uint32_t gpiobase= NRF_GPIO_BASE;
+  NRF_GPIO_Type *gpiobase= (NRF_GPIO_Type *)NRF_GPIO_BASE;
 
-    uBit.init();
-    uBit.serial.baud(115200);
-    uBit.serial.send("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
-    printf("%08x\r\n", gpiobase);
-    uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
+  uBit.init();
+  uBit.serial.baud(115200);
+  uBit.serial.send("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
+  printf("Base %08lx\r\n", uint32_t(gpiobase));
+  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
+  printf("%08lx\r\n", gpiobase->DIR);
+  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
+  printf("%08lx\r\n", gpiobase->IN);
+  printf("Set LED directions to output\r\n");
+  gpiobase->DIRSET= 0xFFF0UL;
+  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
+  printf("%08lx\r\n", gpiobase->DIR);
+  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
+  printf("%08lx\r\n", gpiobase->IN);
+  uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
