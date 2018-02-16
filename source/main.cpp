53d4e6e9fe50dc92ea1425069a3417c39c71a1bc
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-16 09:13:02 +0100

    Absolute bare bones main routine, only blinks LEDs

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,19 @@
 /**
- * This is a simple template for use with Calliope mini.
+ * This is an minimialistic Proof-of-Concept
+ * of direct register level programming
+ * that bypasses all higher level libraries and runtimes
  *
- * @copyright (c) Calliope gGmbH.
- * @author Matthias L. Jugel.
+ * @copyright (c) Christoph Maier
+ * @author Christoph Maier.
  *
  * Licensed under the Apache License 2.0
  */
 
-#include <MicroBit.h>
+#include <nrf51.h>
 
-MicroBit uBit;
-
-int main(void) {
+int main(void)
+{
   NRF_GPIO_Type *gpiobase= (NRF_GPIO_Type *)NRF_GPIO_BASE;
-
-  uBit.init();
-  uBit.serial.baud(115200);
-  uBit.serial.send("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
-  printf("Base %08lx\r\n", uint32_t(gpiobase));
-  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-  printf("%08lx\r\n", gpiobase->DIR);
-  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-  printf("%08lx\r\n", gpiobase->IN);
 
   for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70dUL);
   for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x505UL);
@@ -32,19 +24,11 @@ int main(void) {
     {
       uint32_t clr= ((~r&7)<<13)|(c<<4); 
       uint32_t set= (r<<13)|((~c&0x1ffUL)<<4);
-      printf("rows: %1lx ", r);
-      printf("cols: %03lx\r\n", c);
-      for (int n= 0; n < 32768; n++)
+      for (uint32_t n= 0; n < 131072; n++)
       {
 	gpiobase->OUTSET= set;
 	gpiobase->OUTCLR= clr;
       }
-      printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-      printf("%08lx\r\n", gpiobase->DIR);
-      printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-      printf("%08lx\r\n", gpiobase->IN);
     }
   }
-
-  uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
