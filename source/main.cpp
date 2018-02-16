1de9807a3e3ee223a9d1bbe2a56cc7b9a8f63d5e
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-16 07:36:57 +0100

    This should light up all LEDs ... but doesn't

--- source/main.cpp
+++ source/main.cpp
@@ -24,12 +24,24 @@ int main(void) {
   printf("%08lx\r\n", gpiobase->IN);
 
   printf("Configure LED pins\r\n");
-  for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70fUL);
-  for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x507UL);
+  for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70dUL);
+  for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x505UL);
+
+  gpiobase->OUTSET= 0x1ff0UL;
+  gpiobase->OUTCLR= 0xe000UL;
+  
+  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
+  printf("%08lx\r\n", gpiobase->DIR);
+  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
+  printf("%08lx\r\n", gpiobase->IN);
+
+  gpiobase->OUTSET= 0xe000UL;
+  gpiobase->OUTCLR= 0x1ff0UL; 
 
   printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
   printf("%08lx\r\n", gpiobase->DIR);
   printf("In   %08lx: ", uint32_t(&gpiobase->IN));
   printf("%08lx\r\n", gpiobase->IN);
+
   uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
