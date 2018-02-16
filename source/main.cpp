623880c0a87af4dc00f4be9eb9acc8ba883bb9f0
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-16 08:36:13 +0100

    All 2^12 blinkencombinations, with serial text output

--- source/main.cpp
+++ source/main.cpp
@@ -23,34 +23,28 @@ int main(void) {
   printf("In   %08lx: ", uint32_t(&gpiobase->IN));
   printf("%08lx\r\n", gpiobase->IN);
 
-  printf("Configure LED pins\r\n");
   for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70dUL);
   for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x505UL);
 
-  gpiobase->OUTSET= 0x1ff0UL;
-  gpiobase->OUTCLR= 0xe000UL;
-  
-  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-  printf("%08lx\r\n", gpiobase->DIR);
-  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-  printf("%08lx\r\n", gpiobase->IN);
-
-  for (uint8_t r= 0; r < (1<<4); r++)
-    for (uint8_t c= 0; c < (1<<10); c++)
+  for (uint32_t r= 1; r < (1<<3); r++)
+  {
+    for (uint32_t c= 0; c < (1<<9); c++)
+    {
+      uint32_t clr= ((~r&7)<<13)|(c<<4); 
+      uint32_t set= (r<<13)|((~c&0x1ffUL)<<4);
+      printf("rows: %1lx ", r);
+      printf("cols: %03lx\r\n", c);
+      for (int n= 0; n < 32768; n++)
       {
-	uint32_t set= ((~r&7)<<13)|(c<<4); 
-	uint32_t clr= (r<<13)|((~c&0x1ff0UL)<<4); 
-	for (int n= 0; n < 32768; n++)
-	  {
-	    gpiobase->OUTSET= set;
-	    gpiobase->OUTCLR= clr;
-	  }
+	gpiobase->OUTSET= set;
+	gpiobase->OUTCLR= clr;
       }
-
-  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-  printf("%08lx\r\n", gpiobase->DIR);
-  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-  printf("%08lx\r\n", gpiobase->IN);
+      printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
+      printf("%08lx\r\n", gpiobase->DIR);
+      printf("In   %08lx: ", uint32_t(&gpiobase->IN));
+      printf("%08lx\r\n", gpiobase->IN);
+    }
+  }
 
   uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
