f0583b4b410758731d1c49237824a8b0a504d87b
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-20 23:20:19 +0100

    No register printf statements Hex file size:  16188 Feb 20 23:17 calliope-project-template.hex

--- source/main.cpp
+++ source/main.cpp
@@ -27,12 +27,6 @@ int main(void) {
   cereal.baud(115200);
   printf("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
 
-  printf("Base %08lx\r\n", uint32_t(gpiobase));
-  printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-  printf("%08lx\r\n", gpiobase->DIR);
-  printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-  printf("%08lx\r\n", gpiobase->IN);
-
   for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70dUL);
   for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x505UL);
 
@@ -42,18 +36,11 @@ int main(void) {
     {
       uint32_t clr= ((~r&7)<<13)|(c<<4); 
       uint32_t set= (r<<13)|((~c&0x1ffUL)<<4);
-      printf("rows: %1lx ", r);
-      printf("cols: %03lx\r\n", c);
       for (volatile uint32_t n= 0; n < 131072; n++);
 
       gpiobase->OUTSET= set;
       gpiobase->OUTCLR= clr;
-      printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
-      printf("%08lx\r\n", gpiobase->DIR);
-      printf("In   %08lx: ", uint32_t(&gpiobase->IN));
-      printf("%08lx\r\n", gpiobase->IN);
     }
   }
-
   printf("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
