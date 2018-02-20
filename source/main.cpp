542d9b3c2cac229ef7a6dbc1c512912a83cf3493
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-20 20:38:42 +0100

    include only MicroBitSerial.h instead of MicroBit.h

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,23 @@
 /**
  * This is a simple template for use with Calliope mini.
  *
- * @copyright (c) Calliope gGmbH.
- * @author Matthias L. Jugel.
+ * @copyright (c) Christoph Maier
+ * @author Christoph Maier.
+ * based on work by Matthias L. Jugel, Calliope gGmbH.
  *
  * Licensed under the Apache License 2.0
  */
 
-#include <MicroBit.h>
+#include <MicroBitSerial.h>
 
-MicroBit uBit;
+MicroBitSerial cereal(USBTX, USBRX);
 
 int main(void) {
   NRF_GPIO_Type *gpiobase= (NRF_GPIO_Type *)NRF_GPIO_BASE;
 
-  uBit.init();
-  uBit.serial.baud(115200);
-  uBit.serial.send("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
+  cereal.baud(115200);
+  cereal.send("Wenn ist das Nurnstuck git und Slotermeyer?\r\n");
+
   printf("Base %08lx\r\n", uint32_t(gpiobase));
   printf("Dir  %08lx: ", uint32_t(&gpiobase->DIR));
   printf("%08lx\r\n", gpiobase->DIR);
@@ -46,5 +47,5 @@ int main(void) {
     }
   }
 
-  uBit.serial.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
+  cereal.send("Ja! Beiherhundt das oder die Flipperwaldt gersput!\r\n");
 }
