1e5a250d557645e514808fbe5e799c206827a3af
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-02-21 21:02:51 +0100

    one LED switched on at a time Hex file size:   9808 Feb 21 20:51 calliope-project-template.hex

--- source/main.cpp
+++ source/main.cpp
@@ -29,9 +29,9 @@ int main(void) {
   for (int p= 4; p < 13; gpiobase->PIN_CNF[p++]= 0x70dUL);
   for (int p= 13; p < 16; gpiobase->PIN_CNF[p++]= 0x505UL);
 
-  for (uint32_t r= 1; r < (1<<3); r++)
+  for (uint32_t r= 1; r < (1<<3); r<<=1)
   {
-    for (uint32_t c= 0; c < (1<<9); c++)
+    for (uint32_t c= 1; c < (1<<9); c<<=1)
     {
       uint32_t clr= ((~r&7)<<13)|(c<<4); 
       uint32_t set= (r<<13)|((~c&0x1ffUL)<<4);
