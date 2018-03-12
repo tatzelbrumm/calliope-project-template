e87fe93694cce210ec18665b89a86470b1b1642f
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-03-12 02:51:44 +0100

    build directory for -G "Ninja"

---
+++ build.Ninja/calliope-mini-classic-gcc-nosd/ym/microbit/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cmaier/Hackerspaces/calliope/calliope-project-template/build/calliope-mini-classic-gcc-nosd/ym/microbit
+# Build directory: /home/cmaier/Hackerspaces/calliope/calliope-project-template/build/calliope-mini-classic-gcc-nosd/ym/microbit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(../microbit-dal)
+subdirs(source)
---
+++ build.Ninja/calliope-mini-classic-gcc-nosd/ym/nrf51-sdk/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cmaier/Hackerspaces/calliope/calliope-project-template/build/calliope-mini-classic-gcc-nosd/ym/nrf51-sdk
+# Build directory: /home/cmaier/Hackerspaces/calliope/calliope-project-template/build/calliope-mini-classic-gcc-nosd/ym/nrf51-sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(source)
