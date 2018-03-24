a4aec38a27cfd95430ef16db875bdb6d710c2b08
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-03-24 01:17:21 +0100

    source/main.cpp :  transmitter works

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,14 @@
-/**
- * This is a simple template for use with Calliope mini.
+/*
+ * radio.cpp
+ * =========
+ * Transmitter side
  *
- * @copyright (c) Calliope gGmbH.
- * @author Matthias L. Jugel.
+ * Compile with 
+ * #define MICROBIT_BLE_ENABLED      0
+ * #define MICROBIT_BLE_PAIRING_MODE 0
+ * in yotta_modules/microbit-dal/inc/core/MicroBitConfig.h
  *
- * Licensed under the Apache License 2.0
+ * see https://lancaster-university.github.io/microbit-docs/ubit/radio/# for details
  */
 
 #include <MicroBit.h>
@@ -12,14 +16,40 @@
 
 MicroBit uBit;
 
+const char OM[]="\nOM\n",
+  MANI[]= "\nMANI\n",
+  PADME[]= "\nPADME\n",
+  HUM[]= "\nHUM\n";
+
+static const char *meditations[4]= {
+  OM, MANI, PADME, HUM
+};
 
 int main(void) {
     uBit.init();
     uBit.serial.baud(115200);
-    printf("\r\nInterrupt registers\r\n");
-    dumpIrqEnables();
-    printf("\r\nClock registers\r\n");
-    dumpClockRegisters();
-    printf("\r\nRadio registers\r\n");
-    dumpRadioRegisters();
+
+    uBit.radio.enable();
+    PacketBuffer b(1);
+    unsigned char count=0;
+ considered_harmful:
+    if (uBit.buttonA.isPressed()) {
+        count++;
+    }
+    else if (uBit.buttonB.isPressed()) {
+        count--;
+    }
+    else
+        goto considered_harmful;
+    count%=4;
+    b[0]= count;
+
+    uBit.radio.datagram.send(b);
+
+    uBit.display.scroll(meditations[count]);
+
+    uBit.serial.send(meditations[count]);
+
+    uBit.sleep(680);
+    goto considered_harmful;
 }
